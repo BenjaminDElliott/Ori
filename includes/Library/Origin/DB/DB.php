10245610322f0fecf8ef3bed25393c6e717e99e3
XC <genration9@gmail.com>
2016-01-18 16:23:06 -0500

    Formatting fixes.

--- includes/Library/Origin/DB/DB.php
+++ includes/Library/Origin/DB/DB.php
@@ -19,45 +19,45 @@ class DB extends \Origin\Utilities\Types\Singleton {
 	* Select back full result set as an array.
 	*/
 	public function Query($sql, array $parameters = null){
-    if($this->Execute($sql, $parameters)){
-      return $this->statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-  }
-	
+		if($this->Execute($sql, $parameters)){
+			return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+		}
+	}
+
 	/*
 	* Select just the first row.
 	*/
 	public function QueryFirstRow($sql, array $parameters = null){
 		if($this->Execute($sql, $parameters)){
-      return $this->statement->fetch(PDO::FETCH_ASSOC);
-    }
+			return $this->statement->fetch(PDO::FETCH_ASSOC);
+		}
 	}
-	
+
 	/*
 	* Select the first column.
 	*/
 	public function QueryFirstColumn($sql, array $parameters = null){
 		if($this->Execute($sql, $parameters)){
-      return $this->statement->fetchAll(PDO::FETCH_COLUMN);
-    }
+			return $this->statement->fetchAll(PDO::FETCH_COLUMN);
+		}
 	}
-	
+
 	/*
 	* Select the first column of the first row.
 	*/
 	public function QueryOne($sql, array $parameters = null){
 		if($this->Execute($sql, $parameters)){
-      return $this->statement->fetch(PDO::FETCH_COLUMN, 0);
-    }
+			return $this->statement->fetch(PDO::FETCH_COLUMN, 0);
+		}
 	}
-	
-		/*
+
+	/*
 	* Inserts a record into the database.
 	*/
 	public function Insert($sql, array $parameters = null){
 		return $this->Execute($sql, $parameters);
 	}
-	
+
 	/*
 	* Updates a record in the database.
 	*/
@@ -80,81 +80,82 @@ class DB extends \Origin\Utilities\Types\Singleton {
 		
 		return $this->Execute($query, $binds);
 	}
-	
+
 	/*
 	* Lookup database connection information from settings file and setup connection.
 	*/
-  public function __construct($database_name = null){
-    if($database_name === null){
-      throw new Exception('Invalid database name passed. Please check the call and try again.');
-    }
-    
-    $this->connection_parameters = Settings::Get('databases')->Values([$database_name]);
-    if(!$this->Connect()){
-      throw new Exception('Unable to connect to database: '.$database_name.' - '.$this->error);
-    }
-  }
-  
+	public function __construct($database_name = null){
+		if($database_name === null){
+			throw new Exception('Invalid database name passed. Please check the call and try again.');
+		}
+
+		$this->connection_parameters = Settings::Get('databases')->Values([$database_name]);
+		if(!$this->Connect()){
+			throw new Exception('Unable to connect to database: '.$database_name.' - '.$this->error);
+		}
+	}
+
 	/*
 	* Connect to the database server.
 	*/
-  private function Connect(){
-    $dsn = sprintf('%s:host=%s;dbname=%s;port=%s',
-      $this->connection_parameters->offsetGet('type'),
-      $this->connection_parameters->offsetGet('hostname'),
-      $this->connection_parameters->offsetGet('username'),
-      $this->connection_parameters->offsetGet('port')
-    );
-    
-    $options = array(
-      PDO::ATTR_PERSISTENT => true,
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    );
-    
-    try {
-      $this->connection = new PDO($dsn, $this->connection_parameters->offsetGet('username'), $this->connection_parameters->offsetGet('password'), $options);
-      return true;
-    } catch(PDOException $e) {
-      $this->error = $e->getMessage();
-      return false;
-    }
-  }
-  
-  /*
+	private function Connect(){
+		$dsn = sprintf('%s:host=%s;dbname=%s;port=%s',
+			$this->connection_parameters->offsetGet('type'),
+			$this->connection_parameters->offsetGet('hostname'),
+			$this->connection_parameters->offsetGet('username'),
+			$this->connection_parameters->offsetGet('port')
+		);
+
+		$options = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+		);
+
+		try {
+			$this->connection = new PDO($dsn, $this->connection_parameters->offsetGet('username'), $this->connection_parameters->offsetGet('password'), $options);
+			return true;
+		} catch(PDOException $e) {
+			$this->error = $e->getMessage();
+			return false;
+		}
+	}
+
+	/*
 	* Execute some SQL.
 	*/
-  private function Execute($sql, array $parameters = null){
-    $this->statement = $this->connection->prepare($sql);
+	private function Execute($sql, array $parameters = null){
+		$this->statement = $this->connection->prepare($sql);
 		if($parameters !== null){
 			foreach($parameters as $key => $value){
 				$this->statement->bindValue($key, $value, $this->GetType($value));
 			}
 		}
-		
-    return $this->statement->execute();
-  }
-  
+
+		return $this->statement->execute();
+	}
+
 	/*
 	* Get type of value.
 	*/
-  private function GetType($value){
-    switch (true) {
-      case is_int($value):
-        $type = PDO::PARAM_INT;
-        break;
-      case is_bool($value):
-        $type = PDO::PARAM_BOOL;
-        break;
-      case is_null($value):
-        $type = PDO::PARAM_NULL;
-        break;
-      default:
-        $type = PDO::PARAM_STR;
-    }
-    
-    return $type;
-  }
-	
+	private function GetType($value){
+		switch (true) {
+			case is_int($value):
+				$type = PDO::PARAM_INT;
+				break;
+			case is_bool($value):
+				$type = PDO::PARAM_BOOL;
+				break;
+			case is_null($value):
+				$type = PDO::PARAM_NULL;
+				break;
+			default:
+				$type = PDO::PARAM_STR;
+				break;
+		}
+
+		return $type;
+	}
+
 	/*
 	* SQL builder dump.
 	*/
@@ -164,12 +165,12 @@ UPDATE
 SET
 	%s
 SQL;
-	
+
 	private static $update_where = <<<'SQL'
  WHERE
 	%s
 SQL;
-	
+
 	private static $set_sql = <<<'SQL'
  %s = :%s
 SQL;
